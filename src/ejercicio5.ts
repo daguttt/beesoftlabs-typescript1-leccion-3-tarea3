b1643bc7f263236800a05a2d1b368dfa638d4d1e
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-06 11:26:38 -0500

    Resolve 5th exercise

---
+++ src/ejercicio5.ts
@@ -0,0 +1,17 @@
+const callback1 = (value: string) => console.log("Esto es un texto:", value);
+const callback2 = (value: number) => console.log("Esto es un numero:", value);
+
+const myPromise5: Promise<number> = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(2);
+  }, 1000);
+});
+
+myPromise5
+  .then((value: number) => {
+    callback2(value); // log: Esto es un numero: 2
+    return 1;
+  })
+  .then((value: number) => {
+    console.log(value); // log: 1
+  });
